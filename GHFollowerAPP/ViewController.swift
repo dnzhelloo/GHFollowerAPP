48e228204967f9df3d6506a4b594acb01572dc20
dnzhelloo <dnzhelloo@icloud.com>
2023-04-28 22:39:07 +0300

    Initial Commit

---
+++ GHFollowerAPP/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  GHFollowerAPP
+//
+//  Created by Deniz on 4/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
+
