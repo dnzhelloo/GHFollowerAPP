b292e1dc110c1d7b80943d1de163ec2563650ab4
dnzhelloo <dnzhelloo@icloud.com>
2023-04-29 21:59:24 +0300

    app navigation setup / UITabBarController

---
+++ GHFollowerAPP/FavoritesListVC.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesListVC.swift
+//  GHFollowerAPP
+//
+//  Created by Deniz on 4/29/23.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBrown
+    }
+}
---
+++ GHFollowerAPP/SearchVC.swift
@@ -0,0 +1,17 @@
+//
+//  SearchVC.swift
+//  GHFollowerAPP
+//
+//  Created by Deniz on 4/29/23.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemRed
+    }
+}
